6ebf193d41bcacc3021bcdf12835a39b12c814bb
Pedro Moura <pmoura@redhat.com>
2022-08-19 20:51:14 -0300

    use build-planets.sh to build all planets and Dockerfile to install dependencies and run apache
    Signed-off-by: Pedro Moura <pmoura@redhat.com>

--- pluto/build-planets.sh
+++ pluto/build-planets.sh
@@ -2,32 +2,20 @@
 
 GALAXY='/pluto/builds'
 
-# for PLANET in design desktop edited  people  quality  security  summer-coding; do
-#     if [ "$PLANET" = "people" ]; then
-#         pushd $GALAXY/$PLANET
-#         pluto build planet.ini -o /var/www/html/ -d /var/www/html/ -t $PLANET
-#         popd
-#     else
-#         mkdir /var/www/html/$PLANET 
-#         ln -s /var/www/html/css-v2 /var/www/html/$PLANET/css-v2
-#         ln -s /var/www/html/images-v2 /var/www/html/$PLANET/images-v2
-#         pushd $GALAXY/$PLANET
-#         pluto build planet.ini -o /var/www/html/$PLANET -d /var/www/html/$PLANET -t $PLANET
-#         popd
-#     fi
-# done
+mkdir -p /root/.pluto/
 
-# Lines below just for testing
-pushd $GALAXY/people
-pluto build planet.ini -o /var/www/html/ -d /var/www/html/ -t people
-popd
-
-# for testing purposes, TODO: fix above loop
-pushd $GALAXY/design
-mkdir /var/www/html/design
-ln -s /var/www/html/css-v2 /var/www/html/design/css-v2
-ln -s /var/www/html/images-v2 /var/www/html/design/images-v2
-pluto build planet.ini -o /var/www/html/design -d /var/www/html/design -t design
-popd
-
-httpd -D FOREGROUND &
+for PLANET in design desktop people security summer-coding; do
+    mv templates/$PLANET /root/.pluto/
+    if [ "$PLANET" = "people" ]; then
+        pushd $GALAXY/$PLANET
+        pluto build planet.ini -o /var/www/html/ -d /var/www/html/ -t $PLANET
+        popd
+    else
+        mkdir /var/www/html/$PLANET 
+        ln -s /var/www/html/css-v2 /var/www/html/$PLANET/css-v2
+        ln -s /var/www/html/images-v2 /var/www/html/$PLANET/images-v2
+        pushd $GALAXY/$PLANET
+        pluto build planet.ini -o /var/www/html/$PLANET -d /var/www/html/$PLANET -t $PLANET
+        popd
+    fi
+done

bba992bf5f50c1a51ed7086192da6cf2283a31ad
Gerard Ryan <gerard@ryan.lt>
2022-11-21 23:57:11 +0000

    Tweak templates to work with all specified feeds

--- pluto/build-planets.sh
+++ pluto/build-planets.sh
@@ -8,10 +8,10 @@ build_dir="../../site"
 
 pushd ${GALAXY}
 pluto -c ../ build people.ini -o ${build_dir} -d ${build_dir} -t planet
-for PLANET in design desktop edited quality security ; do # TODO: add summer-coding once it works
+for PLANET in design desktop edited quality security summer-coding ; do
     mkdir -p ${build_dir}/${PLANET}
     cp -r ${build_dir}/css-v2 ${build_dir}/${PLANET}/css-v2
     cp -r ${build_dir}/images-v2 ${build_dir}/${PLANET}/images-v2
-    pluto -c ../ build ${PLANET}.ini -o ${build_dir}/${PLANET} -d ${build_dir}/${PLANET} -t planet
+    pluto -c ../ build ${PLANET//-/}.ini -o ${build_dir}/${PLANET} -d ${build_dir}/${PLANET} -t planet
 done
 popd
